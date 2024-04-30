39a97682399f858e0ca77412dbb9495189eca5c9
julleibibjb <103716240+julleibibjb@users.noreply.github.com>
2024-04-30 10:20:27 +0300

    Update settings.py

--- loan_sys_backend/settings.py
+++ loan_sys_backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://borrowlend.onrender.com/']
+ALLOWED_HOSTS = ['https://borrowlend.onrender.com/','djangop20.pythonanywhere.com']
 
 
 # Application definition
